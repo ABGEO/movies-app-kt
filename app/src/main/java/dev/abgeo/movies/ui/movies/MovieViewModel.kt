8cdce791ab6fa76c54893f38a9a50a49061e3e82
Temuri Takalandze <takalandzet@gmail.com>
2020-12-15 15:36:14 +0400

    :sparkles: Implement progress bar on loading.

--- app/src/main/java/dev/abgeo/movies/ui/movies/MovieViewModel.kt
+++ app/src/main/java/dev/abgeo/movies/ui/movies/MovieViewModel.kt
@@ -18,6 +18,14 @@ class MovieViewModel : ViewModel() {
     val personLiveData: LiveData<List<Movie>>
         get() = _moviesLiveData
 
+    private val _loaderLiveData = MutableLiveData<Boolean>()
+    val loaderLiveData: LiveData<Boolean>
+        get() = _loaderLiveData
+
+    private val _errorLiveData = MutableLiveData<String>()
+    val errorLiveData: LiveData<String>
+        get() = _errorLiveData
+
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://my-json-server.typicode.com/nikoloz14/movies-db/")
         .client(
@@ -32,16 +40,25 @@ class MovieViewModel : ViewModel() {
     private val movieService = retrofit.create(MovieService::class.java)
 
     fun getMovies() {
+        _loaderLiveData.postValue(true)
+
         movieService.getMovies().enqueue(object : Callback<List<Movie>> {
             override fun onResponse(call: Call<List<Movie>>, response: Response<List<Movie>>) {
+                _loaderLiveData.postValue(false)
+
                 if (response.isSuccessful) {
                     response.body()?.let {
                         _moviesLiveData.postValue(it)
                     }
+                } else {
+                    _errorLiveData.postValue("Error Occurred")
                 }
             }
 
-            override fun onFailure(call: Call<List<Movie>>, t: Throwable) {}
+            override fun onFailure(call: Call<List<Movie>>, t: Throwable) {
+                _loaderLiveData.postValue(false)
+                _errorLiveData.postValue("Network Error")
+            }
         })
     }
 
--- app/src/main/java/dev/abgeo/movies/ui/movies/MoviesFragment.kt
+++ app/src/main/java/dev/abgeo/movies/ui/movies/MoviesFragment.kt
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import dev.abgeo.movies.R
 
 class MoviesFragment : Fragment() {
@@ -16,15 +20,24 @@ class MoviesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_movie_list, container, false)
+        val list = view.findViewById<RecyclerView>(R.id.rvMovies)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
         val viewModel = MovieViewModel()
 
         viewModel.personLiveData.observe(this, {
-            if (view is RecyclerView) {
-                with(view) {
-                    layoutManager = LinearLayoutManager(context)
-                    adapter = MovieRecyclerViewAdapter(it, context)
-                }
+            with(list) {
+                layoutManager = LinearLayoutManager(context)
+                adapter = MovieRecyclerViewAdapter(it, context)
             }
+        })
+
+        viewModel.errorLiveData.observe(this, {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT)
+                .show()
+        })
+
+        viewModel.loaderLiveData.observe(this, {
+            progressBar.visibility = if (it) VISIBLE else GONE
         })
 
         viewModel.getMovies()
